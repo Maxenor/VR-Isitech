561a1714f58d3292d5a0ca37a0cd16f455c9aaaf
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-06-13 09:00:49 +0200

    feat: je casse tout

--- VR-Isitech/Assets/GrabHandler.cs
+++ VR-Isitech/Assets/GrabHandler.cs
@@ -5,8 +5,5 @@ public class GrabHandler : MonoBehaviour
     public void onGrab() {
         Debug.Log("onGrab");
 
-        InventoryManager inventoryManager = FindObjectOfType<InventoryManager>();
-
-        inventoryManager.RemoveItem();
     }
 }
